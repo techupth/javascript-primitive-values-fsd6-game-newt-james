9c90c7ecd0d5ab71fcfde0410a22743d95e7ead5
Thanasak <ma.thanasak@gmail.com>
2024-05-12 10:38:29 +0700

    featfeat: Do Example 1-8 Rev.02

--- ex-5.js
+++ ex-5.js
@@ -2,6 +2,6 @@
 
 // Start coding here
 let totalPrice = 27000;
-let discountPrice = 27000 - 27000 * 0.25;
+let discountPrice = totalPrice - 27000 * 0.25;
 let afterDiscountPrice = discountPrice;
 console.log(afterDiscountPrice);
